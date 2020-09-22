d88199694e5aec4ad38e1e6853fcaab202b86403
yuzumone <yuzumone@outlook.com>
2020-09-22 11:33:10 +0900

    Add IPv6Address

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,3 +3,27 @@ Iterable<int> range(int low, int high) sync* {
     yield i;
   }
 }
+
+Iterable<int> stepRange(int a, [int stop, int step]) {
+  int start;
+
+  if (stop == null) {
+    start = 0;
+    stop = a;
+  } else {
+    start = a;
+  }
+
+  if (step == 0) {
+    throw Exception('Step cannot be 0');
+  }
+
+  if (step == null) {
+    start < stop ? step = 1 : step = -1;
+  }
+
+  return start < stop == step > 0
+      ? List<int>.generate(
+          ((start - stop) / step).abs().ceil(), (int i) => start + (i * step))
+      : [];
+}

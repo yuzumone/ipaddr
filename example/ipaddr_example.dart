8537278e7d8edb874a3d789ef779c44dc4463e0f
yuzumone <yuzumone@outlook.com>
2020-09-24 16:28:21 +0900

    Update example

--- example/ipaddr_example.dart
+++ example/ipaddr_example.dart
@@ -36,4 +36,20 @@ void main() {
       BigInt.parse('42541956123769884636017138956568135816'));
   print(v6address); // 2001:4860:4860::8888
   print(v6address + 1); // 2001:4860:4860::8889
+
+  var v6network = ipaddr.IPv6Network('dead:beef::/126');
+  print(v6network); // dead:beef::/126
+  print(v6network.networkAddress); // dead:beef::
+  print(v6network.broadcastAddress); // dead:beef::3
+  print(v6network.prefixlen); // 127
+  print(v6network.netmask); // ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffc
+  print(v6network.hostmask); // ::3
+  print(v6network.hosts); // [dead:beef::1, dead:beef::2]
+  print(v6network
+      .addresses); // [dead:beef::, dead:beef::1, dead:beef::2, dead:beef::3]
+  print(v6network.numAddresses); // 4
+  print(v6network.withPrefixlen); // dead:beef::/126
+  print(v6network
+      .withNetmask); // dead:beef::/ffff:ffff:ffff:ffff:ffff:ffff:ffff:fffc
+  print(v6network.withHostmask); // dead:beef::/::3
 }
